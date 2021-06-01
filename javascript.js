918938dc86421536ce946fb9815acbeb0e37476f
Yasiro <82701620+kamuYasiro@users.noreply.github.com>
2021-06-01 13:40:28 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+$(function(){
+  
+  $('#button').click(function(){
+    if($('.naviul').hasClass('block')){
+
+      $('#navi').fadeOut();
+      $('.naviul').removeClass('block');
+      $('#navi').removeClass('open');
+      $('.mask').css('display','none');
+      $('.span').css('background-color','black');
+      $('.humberger').removeClass('open');
+    }else{
+
+      $('#navi').fadeIn();
+      $('.naviul').addClass('block');
+      $('#navi').addClass('open');
+      $('.mask').css('display','block');
+      $('.span').css('background-color','white');
+      $('.humberger').addClass('open');
+    }
+  });
+
+  $('#alert').click(function(){
+    alert("お疲れ様です");
+  });
+
+
+});
